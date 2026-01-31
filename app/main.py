3f9052d36aa3754d613e7b5e1926398e10a72ba1
Jon <jonathan@marinachain.io>
2026-01-31 15:51:19 +0800

    fix: update CORS to allow multiple origins

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.CORS_ORIGIN],
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
